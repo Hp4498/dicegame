3014f97d42a16e02740dc090f370ef92d4887581
Haripriya <55921828+Hp4498@users.noreply.github.com>
2022-08-22 18:37:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function dice() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-  var randomDiceImage = "Dice"+randomNumber1+".png"; //dice1.png - dice6.png
-
-  var randomImageSource = "images/"+randomDiceImage; //images/dice1.png - images/dice6.png
+  var randomImageSource = "images/Dice"+randomNumber1+".png"; //images/dice1.png - images/dice6.png
 
   var image1 = document.querySelectorAll("img")[0];
 
